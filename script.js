0f8535d4ef48648c95873f503427879f18927927
tanvirislam <mstanvirislam05@gmail.com>
2025-10-22 00:23:50 +0600

    finished

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
+const removeActive = ()=>{
+    const btnac = document.getElementsByClassName("active");
+    for(let button of btnac){
+        button.classList.remove("active")
+    }
+}
 const loadCategory = () => {
     fetch('https://openapi.programming-hero.com/api/levels/all')
         .then(res => res.json())
         .then(data => {
+            
             displayCat(data.data);
 
         })
@@ -9,21 +16,29 @@ const loadCategory = () => {
 
 const displayCat = (data) => {
     data.forEach(e => {
+        console.log(e.id);
+        
         const catContainer = document.getElementById("lesson");
         const div = document.createElement("div");
         div.innerHTML = `
-        <button onclick="loadContent(${e.level_no})" class="btn border-[#422AD5] text-[#422AD5] hover:text-white hover:bg-[#422AD5]"><img src="assets/fa-book-open.png" alt="">Lesson-${e.level_no}</button>
+        <button id="btn-${e.id}"  onclick="loadContent(${e.level_no}); forId(${e.id})" class=" btn border-[#422AD5] text-[#422AD5] hover:text-white hover:bg-[#422AD5]"><img src="assets/fa-book-open.png" alt="">Lesson-${e.level_no}</button>
         `
         catContainer.appendChild(div)
 
     });
 
 }
+const forId = (id)=>{
+            const clickBtn = document.getElementById(`btn-${id}`)
+            removeActive();
+            clickBtn.classList.add("active")
+}
 const loadContent = (id) => {
     const url = `https://openapi.programming-hero.com/api/level/${id}`;
     fetch(url)
         .then(res => res.json())
         .then(data => {
+            
             displayWord(data.data);
 
         })
@@ -43,15 +58,16 @@ const displayWord = (data) => {
             return;
         }
     data.forEach(element => {
+        
         const div = document.createElement("div");
         div.innerHTML = `
     <div class="card bg-base-100 w-96 shadow-sm text-center">
         <div class="card-body">
           <h2 class="text-3xl font-bold">${element.word}</h2>
           <h2 class="text-2xl">Meaning/Pronounciation</h2>
-          <h1 class="text-3xl font-bold">${element.meaning} ${element.pronunciation}</h1>
+          <h1 class="text-3xl font-bold">${element.meaning} / ${element.pronunciation}</h1>
           <div class="flex justify-between p-5">
-            <img class="h-7" src="https://img.icons8.com/ios/50/about.png" alt="about">
+            <img onclick="details(${element.id})" class="h-7" src="https://img.icons8.com/ios/50/about.png" alt="about">
             <img class="h-7" src="https://img.icons8.com/ios/50/room-sound.png" alt="sound">
           </div>
         </div>
@@ -62,7 +78,40 @@ const displayWord = (data) => {
 
 
 }
+const details = (id)=>{
+    fetch(`https://openapi.programming-hero.com/api/word/${id}`)
+    .then(res=>res.json())
+    .then(data=>{
+    displayDetails(data.data)
+        
+    })
+}
+const displayDetails=(data)=>{
+    console.log(data);
+    document.getElementById('my_modal_5').showModal()
+    
+const main = document.getElementById("modal");
+    main.innerHTML=`
 
+    <h3 class="text-lg font-bold">${data.word}</h3>
+    <h2 class="text-xs font-semibold">Meaning</h2>
+    <h3>${data.meaning}</h3>
+    <h2 class="text-xs font-semibold">Example</h2>
+    <h2 class="text-xs font-normal text-gray-500">${data.sentence}</h2>
+    <h3>সমার্থক শব্দ গুলো</h3>
+    <div class="felx ">
+      <div class="btn ">${data.synonyms}</div>
+    </div>
+     <div class="modal-action">
+      <form method="dialog">
+        <!-- if there is a button in form, it will close the modal -->
+        <button class="btn">Close</button>
+      </form>
+    </div>
+    `
+
+
+}
 loadCategory()
 
 // {
@@ -76,4 +125,15 @@ loadCategory()
 //     "word": "Cautious",
 //     "meaning": "সতর্ক",
 //     "pronunciation": "কশাস"
+// }
+
+// {
+//     "word": "Abundant",
+//     "meaning": null,
+//     "pronunciation": "অবানডান্ট",
+//     "level": 3,
+//     "sentence": "Water is abundant in rainy seasons.",
+//     "points": 3,
+//     "synonyms": [],
+//     "id": 1
 // }
